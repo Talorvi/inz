c9d3e1980b0fe8cda66db05e315c9aaab6580a0d
Lukasz Matysik <lukasz.matysik6@gmail.com>
2019-10-21 19:46:07 +0200

    Creating simple route

---
+++ src/components/auth/LoginForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>LoginForm</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'LoginForm',
+    data() {
+      return {
+        form: {
+          name: '',
+          password: '',
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .clearfix:before,
+  .clearfix:after {
+    display: table;
+    content: "";
+  }
+  .clearfix:after {
+    clear: both
+  }
+</style>
---
+++ src/layouts/Login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="login">
+    <router-link to="/">Home</router-link>
+    <div><LoginForm /></div>
+  </div>
+</template>
+
+<script>
+  import LoginForm from '../components/auth/LoginForm.vue'
+  export default {
+    name: 'login',
+    components: {
+      LoginForm
+    }
+  }
+</script>
