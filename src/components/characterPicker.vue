f41c5dc531317931ec26f43cffffa766ad833b75
Łukasz Matysik <lukasz.matysik6@gmail.com>
2019-11-15 16:10:21 +0100

    Character picking handled without deletion completed yet

--- src/components/characterPicker.vue
+++ src/components/characterPicker.vue
@@ -11,22 +11,38 @@
           <q-item-label>Item with caption</q-item-label>
           <q-item-label caption>Caption</q-item-label>
         </q-item-section>
-        <q-btn size="12px" flat dense round icon="delete" v-on:click="deleteCharacter(character)" />
+        <q-btn
+          size="12px"
+          flat
+          dense
+          round
+          icon="delete"
+          v-on:click="deleteCharacter(character)"
+        />
       </q-item>
     </q-list>
 
     <q-list bordered separator v-else>
       <q-item
-        active-class="bg-teal-1 text-grey-8"
+        active-class="bg-teal-1"
         clickable
         v-ripple
         v-for="character in characters"
         :key="character.name"
         :active="character.selected"
-        v-on:click="clickedOnItem(character)"
       >
-        <q-item-section>{{ character.name }}</q-item-section>
-        <q-btn size="12px" flat dense round icon="delete"  v-on:click="deleteCharacter(character)" />
+        <q-item-section v-on:click="clickedOnItem(character)">{{
+          character.name
+        }}</q-item-section>
+        <q-btn
+          size="12px"
+          flat
+          dense
+          round
+          icon="delete"
+          color="black"
+          v-on:click="deleteCharacter(character)"
+        />
       </q-item>
     </q-list>
   </div>
@@ -36,51 +52,62 @@
 export default {
   data() {
     return {
-      selectedCharacter: null,
-      characters: [
-        { name: "alfa", selected: false },
-        { name: "beta", selected: false },
-        { name: "gamma", selected: false },
-        { name: "delta", selected: false }
-      ],
       isGameMaster: false
     };
   },
-  computed() {},
+
+  beforeCreate() {
+    this.$store.dispatch("reloadCharacters", {
+      data: this.$q
+    });
+  },
+  computed: {
+    characters() {
+      return this.$store.getters.getCharacterNameList;
+    }
+  },
+
   methods: {
     clickedOnItem(character) {
-      if (this.selectedCharacter == null) {
-        character.selected = !character.selected;
-        this.selectedCharacter = character;
-      } else if(this.selectedCharacter === character){
+      console.log("before:" + this.$store.getters.getSelectedCharacter.name);
+      if (character.name === this.$store.getters.getSelectedCharacter.name) {
         console.log("You cannot unselect character");
-      }
-      else{
-        this.selectedCharacter.selected = false;
-        this.selectedCharacter = character;
-        character.selected = true;
-      }
-
-      //setChosenCharacter
-      //setActiveToElementList
-    },
-    deleteCharacter(character){
-      for( var i = 0; i < this.characters.length; i++){
-        if ( this.characters[i].name === character.name) {
-          if (confirm("Are you sure you want to delete this character?")) {
-            //send request to remove character
-            //send request to retreive character
+      } else {
+        var prevIndex = 0;
+        for (
+          var i = 0;
+          i < this.$store.getters.getCharacterNameList.length;
+          i++
+        ) {
+          if (
+            this.$store.getters.getCharacterNameList[i].name ===
+            this.$store.getters.getSelectedCharacter.name
+          ) {
+            prevIndex = i;
           }
-
+        }
+        this.$store.commit("unselectCharacter", {
+          index: character.index,
+          prevIndex: prevIndex
+        });
+      }
+      console.log("after:" + this.$store.getters.getSelectedCharacter.name);
+    },
+    deleteCharacter(character) {
+      console.log(this.games);
+      for (var i = 0; i < this.characters.length; i++) {
+        if (this.characters[i].name === character.name) {
+          console.log(character.name);
+          if (confirm("Are you sure you want to delete this character?")) {
+            this.$store.dispatch("requestDeleteCharacter", {
+              data: this.$q,
+              scenarioKey: "TESTSCEN",
+              characterName: character.name
+            });
+          }
         }
       }
     }
-  },
-  mounted() {
-    //get list of characters
-    //get list players
-    //check if you are GM
-
   }
 };
 </script>
