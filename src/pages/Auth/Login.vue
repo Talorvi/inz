8dad71e7c21bf48c97ad3e92bc9483404169090a
Mateusz Siedlarz <msiedlarz@netizens.pl>
2019-11-18 10:57:28 +0100

    Aktualizacja panelu logowania

--- src/pages/Auth/Login.vue
+++ src/pages/Auth/Login.vue
@@ -1,8 +1,13 @@
 <template>
-  <div class="q-pa-md form">
+  <div>
+    <div class="row bg-primary justify-center">
+      <div class="col-xs-12 col-sm-8">
+        <h3 class="text-accent q-pl-lg">Log in</h3>
+      </div>
+    </div>
     <div class="row justify-center">
       <div class="col-12 col-md-8 ">
-        <LoginForm />
+        <LoginForm class="q-pl-lg q-pt-lg" />
       </div>
     </div>
   </div>
@@ -18,8 +23,3 @@ export default {
 };
 </script>
 
-<style scoped>
-.form {
-  margin-top: 2em;
-}
-</style>
